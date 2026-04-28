777f76b7be5988c9a089828aa6b3c7339fe0858d
Maream-salh26 <salehmaream1@gmail.com>
2026-04-28 11:55:18 +0300

    Update prog.c

--- Assignment1/Assignment1/prog.c
+++ Assignment1/Assignment1/prog.c
@@ -1,55 +1,57 @@
+
 #include <stdio.h>
 #include <string.h>
 
-#define N 15
+#define LIMIT 25
 
-struct myBuffer {
-    char data[N];
-    int s;
-    int e;
-    int count;
+struct AppData {
+    char info[LIMIT];
+    int p1;
+    int p2;
+    int total;
 };
 
-void init(struct myBuffer *b) {
-    b->s = 0;
-    b->e = 0;
-    b->count = 0;
+void start_storage(struct AppData *a) {
+    a->p1 = 0;
+    a->p2 = 0;
+    a->total = 0;
 }
 
-void add(struct myBuffer *b, char c) {
-    if (b->count < N) {
-        b->data[b->e] = c;
-        b->e = (b->e + 1) % N;
-        b->count++;
+void push_val(struct AppData *a, char v) {
+    if (a->total < LIMIT) {
+        a->info[a->p2] = v;
+        a->p2 = (a->p2 + 1) % LIMIT;
+        a->total++;
     }
 }
 
-char get(struct myBuffer *b) {
-    if (b->count > 0) {
-        char v = b->data[b->s];
-        b->s = (b->s + 1) % N;
-        b->count--;
-        return v;
-    }
-    return '\0';
+char pull_val(struct AppData *a) {
+    if (a->total == 0) return '\0';
+    char v = a->info[a->p1];
+    a->p1 = (a->p1 + 1) % LIMIT;
+    a->total--;
+    return v;
 }
 
 int main() {
-    struct myBuffer b;
-    init(&b);
-    
-    char txt[] = "Maryam-CE-ESY";
-    int i;
-    
-    for(i = 0; i < strlen(txt); i++) {
-        add(&b, txt[i]);
+    struct AppData my_data;
+    start_storage(&my_data);
+
+    char str_input[30];
+    printf("Enter name: ");
+    scanf("%s", str_input);
+
+    strcat(str_input, "-CE-ESY");
+
+    for (int i = 0; i < strlen(str_input); i++) {
+        push_val(&my_data, str_input[i]);
     }
-    
-    printf("Result: ");
-    while(b.count > 0) {
-        printf("%c", get(&b));
+
+    printf("Output: ");
+    while (my_data.total > 0) {
+        printf("%c", pull_val(&my_data));
     }
     printf("\n");
-    
+
     return 0;
 }
